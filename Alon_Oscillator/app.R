8c8a1a2a989d14ac094d70a1e3bca3baa33b02c1
Ian Coccimiglio <icoccimi@gmail.com>
2023-11-23 16:53:03 -0800

    Updated for submission

--- Alon_Oscillator/app.R
+++ Alon_Oscillator/app.R
@@ -11,6 +11,10 @@ library(shiny)
 library(cOde)
 library(deSolve)
 
+# Feature 1, Creating a damped biological oscillator. This compiles an object which
+# simulates a biological system that tends towards damped oscillation.
+# Users can change the INDIVIDUAL parameters and the simulation time.
+# Outcomes are changes in the phase-space diagram and the time-concentration plot.
 osc2 <- cOde::funC(
     c(
         X = "-b1*Y - a1*X",
@@ -18,6 +22,11 @@ osc2 <- cOde::funC(
     ), modelname = "Alon_damped", compile=TRUE
 )
 
+## Feature 2, Creating a 3-part repressilator. This compiles an object which
+# simulates a 3 component system that tends towards infinite oscillations.
+# Users are able to change the global degradation, creation, and concentration constants.
+# Additionally, they can change the simulation length.
+# As above, this produces a phase-space diagram and the time-concentration plot.
 osc3 <- cOde::funC(
     c(
         # 1 and 3 are rate constants for the purposes of this demo.
@@ -28,6 +37,11 @@ osc3 <- cOde::funC(
     modelname = "Osc_3", compile=TRUE
 )
 
+## Feature 3, Creating the brusselator This compiles an object which
+# simulates a 2 component system that tends towards infinite oscillations.
+# Users are able to manipulate the different inflows and outflow rates,
+# as well as concentrations of products A, and B
+# As above, this produces a phase-space diagram and the time-concentration plot.
 brussC <- cOde::funC(
     c(
         X = "k1*A - k2*B*X + k3*(X^2)*Y - k4*X",
@@ -84,7 +98,7 @@ ui <- fluidPage(
                      sliderInput("k2", "Catalytic Rate B (k2)", min = 1, max = 20, value = 1),
                      sliderInput("k3", "XY binding rate (k3)", min = 1, max = 20, value = 1),
                      sliderInput("k4", "X degradation rate (k4)", min = 1, max = 20, value = 1),
-                     sliderInput("len_3", "Simulation Time", min = 1, max = 50, value = 10)
+                     sliderInput("len_3", "Simulation Time", min = 1, max = 200, value = 50)
                  ),
 
                  # Show a plot of the generated distribution
@@ -115,7 +129,7 @@ server <- function(input, output) {
 
     output$brussel <- renderImage({
         list(src = "Brusselator.png",
-             width = 200,
+             width = 250,
              height = 250)
     }, deleteFile = FALSE)
 
@@ -125,9 +139,14 @@ server <- function(input, output) {
         k_list <- c(b1=input$beta1, b2=input$beta2, a1=input$alpha1, a2=input$alpha2)
         step=0.01
         damp_osc <- odeC(y=y0, times=seq(0,input$len,step), func=osc2, parms = k_list)
-        plot(damp_osc[,1], damp_osc[,2], type='l')
+        plot(damp_osc[,1], damp_osc[,2], type='l',
+             xlab = "Time",
+             ylab = "Concentration")
         lines(damp_osc[,1], damp_osc[,3], col='red', type='l')
-        plot(damp_osc[,2], damp_osc[,3], type='l')
+        plot(damp_osc[,2], damp_osc[,3], type='l',
+             xlab = "Concentration X",
+             ylab = "Concentration Y"
+             )
     }
     )
 
@@ -140,10 +159,15 @@ server <- function(input, output) {
         k_list_2 <- c(beta=input$Beta, k=input$k, g2=input$Gamma)
         step=0.2
         osc_3 <- odeC(y=y0_2, times=seq(0,input$len_2,step), func=osc3, parms = k_list_2)
-        plot(osc_3[,1], osc_3[,2], col='black', type='l')
+        plot(osc_3[,1], osc_3[,2], col='black', type='l',
+             xlab = "Time",
+             ylab = "Concentration")
         lines(osc_3[,1], osc_3[,3], col='red')
         lines(osc_3[,1], osc_3[,4], col='blue')
-        plot(osc_3[,2], osc_3[,3], col='black', type='l')
+        plot(osc_3[,2], osc_3[,3], col='black', type='l',
+             xlab = "Concentration X",
+             ylab = "Concentration Y"
+             )
     }
     )
 
@@ -155,9 +179,13 @@ server <- function(input, output) {
         k_list3 <- c(A=input$A, B=input$B, k1=input$k1, k2=input$k2, k3=input$k3, k4=input$k4)
         step=0.1
         ode_bruss <- odeC(y=y0_3, times=seq(0,input$len_3,step), func=brussC, parms = k_list3)
-        plot(ode_bruss[,1], ode_bruss[,2],col='black', type='l',ylim=c(0,8))
+        plot(ode_bruss[,1], ode_bruss[,2],col='black', type='l',ylim=c(0,8),
+             xlab = "Time",
+             ylab = "Concentration")
         lines(ode_bruss[,1], ode_bruss[,3],col='blue')
-        plot(ode_bruss[,2], ode_bruss[,3],type='l')
+        plot(ode_bruss[,2], ode_bruss[,3],type='l',
+             xlab = "Concentration X",
+             ylab = "Concentration Y")
     }
     )
 }
