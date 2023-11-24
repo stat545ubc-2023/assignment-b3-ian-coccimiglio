1af5390e8785457ac660738948136fb85c81adbc
Ian Coccimiglio <icoccimi@gmail.com>
2023-11-23 16:29:38 -0800

    First commit for README and model

---
+++ Alon_Oscillator/app.R
@@ -0,0 +1,166 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(cOde)
+library(deSolve)
+
+osc2 <- cOde::funC(
+    c(
+        X = "-b1*Y - a1*X",
+        Y = "b2*X - a2*Y"
+    ), modelname = "Alon_damped", compile=TRUE
+)
+
+osc3 <- cOde::funC(
+    c(
+        # 1 and 3 are rate constants for the purposes of this demo.
+        X = "beta/(1+(Z/k)^3) - g2*X",
+        Y = "beta/(1+(X/k)^3) - g2*Y",
+        Z = "beta/(1+(Y/k)^3) - g2*Z"
+    ),
+    modelname = "Osc_3", compile=TRUE
+)
+
+brussC <- cOde::funC(
+    c(
+        X = "k1*A - k2*B*X + k3*(X^2)*Y - k4*X",
+        Y = "k2*B*X - k3*(X^2)*Y"
+    ), modelname = "brusselC", compile=TRUE
+)
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    titlePanel("Biological Oscillations"),
+    tabsetPanel(
+        tabPanel("Damped Biological Oscillator",
+            sidebarLayout(
+                sidebarPanel(
+                    sliderInput("alpha1", "Degradation X (\u03B11)", min = 1, max = 20, value = 1),
+                    sliderInput("beta1", "Formation X (\u03B21)", min = 1, max = 20, value = 8),
+                    sliderInput("alpha2", "Degradation Y (\u03B12)", min = 1, max = 20, value = 1),
+                    sliderInput("beta2", "Formation Y (\u03B22)", min = 1, max = 20, value = 8),
+                    sliderInput("len", "Simulation Time", min = 1, max = 50, value = 6)
+                ),
+
+                mainPanel(
+                         plotOutput("dampOsc"),
+                         imageOutput("damp"),
+                )
+            )
+        ),
+
+        tabPanel("Repressilator",
+                 sidebarLayout(
+                     sidebarPanel(
+                         sliderInput("Beta", "Protein Generation Rate (\u03B2)", min = 0, max = 20, value = 1),
+                         sliderInput("k", "Protein Concentration Factor", min = 1, max = 20, value = 1),
+                         sliderInput("Gamma", "Protein Degradation Rate (\u03B3)", min = 0, max = 5, value = 0.1, step=0.1),
+                         sliderInput("len_2", "Simulation Length", min = 1, max = 1000, value = 300)
+                     ),
+
+                     # Show a plot of the generated distribution
+                     mainPanel(
+                         plotOutput("Osc3"),
+                         imageOutput("repress")
+                     )
+                 )
+        ),
+
+
+        tabPanel("Brusselator",
+             sidebarLayout(
+                 sidebarPanel(
+                     sliderInput("A", "Autocatalytic A", min = 1, max = 20, value = 1),
+                     sliderInput("B", "Autocatalytic B", min = 1, max = 20, value = 3),
+                     sliderInput("k1", "Catalytic Rate A (k1)", min = 1, max = 20, value = 1),
+                     sliderInput("k2", "Catalytic Rate B (k2)", min = 1, max = 20, value = 1),
+                     sliderInput("k3", "XY binding rate (k3)", min = 1, max = 20, value = 1),
+                     sliderInput("k4", "X degradation rate (k4)", min = 1, max = 20, value = 1),
+                     sliderInput("len_3", "Simulation Time", min = 1, max = 50, value = 10)
+                 ),
+
+                 # Show a plot of the generated distribution
+                 mainPanel(
+                     plotOutput("Brussel"),
+                     imageOutput("brussel")
+                 )
+             )
+        )
+
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$damp <- renderImage({
+        list(src = "Damped_Oscillator.png",
+        width = 200,
+        height = 200)
+    }, deleteFile = FALSE)
+
+    output$repress <- renderImage({
+        list(src = "Repressilator.png",
+             width = 200,
+             height = 250)
+    }, deleteFile = FALSE)
+
+    output$brussel <- renderImage({
+        list(src = "Brusselator.png",
+             width = 200,
+             height = 250)
+    }, deleteFile = FALSE)
+
+    output$dampOsc <- renderPlot({
+        par(mfrow=c(1,2))
+        y0 <- c(X=1,Y=1)
+        k_list <- c(b1=input$beta1, b2=input$beta2, a1=input$alpha1, a2=input$alpha2)
+        step=0.01
+        damp_osc <- odeC(y=y0, times=seq(0,input$len,step), func=osc2, parms = k_list)
+        plot(damp_osc[,1], damp_osc[,2], type='l')
+        lines(damp_osc[,1], damp_osc[,3], col='red', type='l')
+        plot(damp_osc[,2], damp_osc[,3], type='l')
+    }
+    )
+
+    output$Osc3 <- renderPlot({
+        par(mfrow=c(1,2))
+        Z_Conc = 3
+        Y_Conc = 2
+        X_Conc = 1
+        y0_2 <- c(X=X_Conc,Y=Y_Conc,Z=Z_Conc)
+        k_list_2 <- c(beta=input$Beta, k=input$k, g2=input$Gamma)
+        step=0.2
+        osc_3 <- odeC(y=y0_2, times=seq(0,input$len_2,step), func=osc3, parms = k_list_2)
+        plot(osc_3[,1], osc_3[,2], col='black', type='l')
+        lines(osc_3[,1], osc_3[,3], col='red')
+        lines(osc_3[,1], osc_3[,4], col='blue')
+        plot(osc_3[,2], osc_3[,3], col='black', type='l')
+    }
+    )
+
+    output$Brussel <- renderPlot({
+        par(mfrow=c(2,1))
+        X_Conc_Bruss <- 2
+        Y_Conc_Bruss <- 2
+        y0_3 <- c(X=X_Conc_Bruss,Y=Y_Conc_Bruss)
+        k_list3 <- c(A=input$A, B=input$B, k1=input$k1, k2=input$k2, k3=input$k3, k4=input$k4)
+        step=0.1
+        ode_bruss <- odeC(y=y0_3, times=seq(0,input$len_3,step), func=brussC, parms = k_list3)
+        plot(ode_bruss[,1], ode_bruss[,2],col='black', type='l',ylim=c(0,8))
+        lines(ode_bruss[,1], ode_bruss[,3],col='blue')
+        plot(ode_bruss[,2], ode_bruss[,3],type='l')
+    }
+    )
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Assignment-B3: Biological Oscillation.
+
+Here, I design and create a series of biological oscillators as
+described in *Uri Alon - An Introduction to Systems Biology* - Chapter
+6. The goal is to show how we can model these dynamical processes in R,
+and allow users to moderate the parameters through an interactive GUI.
+
+The system is solved using the deSolve and cOde packages, which allow
+for fast solving and compilation of systems of ordinary differential
+equations.
+
+First, we build a damped oscillator, which is a two-component system. It
+comes with a dynamic user interface to edit the simulation time and the
+individual rate parameters.
+
+Next, we build an undamped multi-component oscillator. This has the
+capacity to oscillate indefinitely, even without noise. Here, we
+showcase a 3-component system, in which each component has negative
+feedback on the following component.
+
+![](README_files/figure-gfm/pressure-1.png)<!-- -->
+
+Finally, we showcase a coupled feedback loop, which has two components,
+but one component is autoregulated with positive feedback. This creates
+oscillations using only two components and without noise.
